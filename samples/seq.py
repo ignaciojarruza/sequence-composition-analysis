99923bec57255be9930a5e778b60414ee92fb292
ignaciojarruza <85847657+ignaciojarruza@users.noreply.github.com>
2024-11-08 20:21:35 -0500

    seq object examples

---
+++ samples/seq.py
@@ -0,0 +1,25 @@
+from Bio.Seq import Seq
+
+# Creating sequence object
+sequence = Seq("AGTACACTGGT")
+
+# .find() example
+sequence.find("ACT") # 5
+sequence.find("TAG") # -1
+
+# .count() example
+sequence.count("A")
+sequence.count("G")
+
+# count_overlap() example
+sequenceOverlap = "AAAA"
+sequenceOverlap.count_overlap("AA") # returns 3 instead of 2 (nonoverlap)
+
+# complement() and reversecomplement()
+sequence.complement()
+sequence.reverse_complement()
+
+# transcribe() and back_transcribe()
+sequence.transcribe()
+sequence.transcribe().back_transcribe()
+
